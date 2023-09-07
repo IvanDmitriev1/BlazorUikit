68342bf3d726a84144ccd440d8225b0f283ee19d
Ivan Dmitriev <42055372+IvanDmitriev1@users.noreply.github.com>
2023-09-07 20:50:12 +0600

    Renamed Link, fixed NumericField

--- UiKit/Components/UiLink.cs
+++ UiKit/Components/UiLink.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Components.Routing;
 
 namespace UiKit.Components;
 
-public sealed class Link : UiText
+public sealed class UiLink : UiText
 {
 	[Parameter, EditorRequired]
 	public string Href { get; set; } = null!;
