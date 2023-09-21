1ce8cd92e2b9afb1096f43d50a6802437440b9f8
Ivan Dmitriev <42055372+IvanDmitriev1@users.noreply.github.com>
2023-09-21 11:23:24 +0600

    Update IconGenerator.cs

--- IconsGenerator/IconGenerator.cs
+++ IconsGenerator/IconGenerator.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Text;
 using System.Xml;
 using Microsoft.CodeAnalysis;
@@ -8,63 +7,73 @@ using Microsoft.CodeAnalysis.Text;
 namespace IconsGenerator;
 
 [Generator(LanguageNames.CSharp)]
-public class IconGenerator : ISourceGenerator
+public class IconGenerator : IIncrementalGenerator
 {
-	public void Initialize(GeneratorInitializationContext context)
+	public void Initialize(IncrementalGeneratorInitializationContext context)
 	{
-		
+		context.RegisterPostInitializationOutput(Generate);
+	}
+
+	private static void Generate(IncrementalGeneratorPostInitializationContext context)
+	{
+		try
+		{
+			var assembly = Assembly.GetExecutingAssembly();
+			var iconAssemblyNames = assembly.GetManifestResourceNames()
+											.Where(static s => s.StartsWith(Path));
+
+			var enumSb = new StringBuilder(81_920);
+			enumSb.AppendLine("namespace TablerIconGenerator;");
+			enumSb.AppendLine("public enum TablerIcon");
+			enumSb.AppendLine("{");
+
+			var extensionsSb = new StringBuilder(81_920 * 6);
+			extensionsSb.AppendLine("namespace TablerIconGenerator;");
+			extensionsSb.AppendLine("public static class TablerIconExtensions");
+			extensionsSb.AppendLine("{");
+
+			var extensionsMethodSb = new StringBuilder(10_000);
+			extensionsMethodSb.AppendLine("public static Microsoft.AspNetCore.Components.RenderFragment ToRenderFragment(this TablerIconGenerator.TablerIcon icon) => icon switch");
+			extensionsMethodSb.AppendLine("{");
+
+			foreach (var assemblyName in iconAssemblyNames)
+			{
+				context.CancellationToken.ThrowIfCancellationRequested();
+
+				var iconFormattedName = GetIconFilteredName(assemblyName);
+
+				if (string.IsNullOrEmpty(iconFormattedName))
+					continue;
+
+				enumSb.Append(iconFormattedName);
+				enumSb.Append(",\n");
+
+				extensionsSb.AppendLine(CreateRenderFragment(assembly, assemblyName, iconFormattedName));
+				extensionsMethodSb.AppendLine($"TablerIconGenerator.TablerIcon.{iconFormattedName} => {iconFormattedName},");
+			}
+
+			enumSb.Remove(enumSb.Length - 2, 2);
+			enumSb.AppendLine("}");
+
+			var enumSource = enumSb.ToString();
+			context.AddSource("TablerIcon.g.cs", SourceText.From(enumSource, Encoding.UTF8));
+
+			extensionsMethodSb.AppendLine("};");
+			extensionsSb.Append(extensionsMethodSb);
+
+			extensionsSb.AppendLine("}");
+			var extensionsSource = extensionsSb.ToString();
+
+			context.AddSource("TablerIconExtensions.g.cs", SourceText.From(extensionsSource, Encoding.UTF8));
+		}
+		catch (OperationCanceledException ) { }
+		catch (Exception e)
+		{
+			//
+		}
 	}
 
 	private const string Path = "IconsGenerator.node_modules._tabler.icons.icons.";
-
-	public void Execute(GeneratorExecutionContext context)
-	{
-		var assembly = Assembly.GetExecutingAssembly();
-		var iconAssrmblyNames = assembly.GetManifestResourceNames()
-								.Where(s => s.StartsWith(Path));
-
-		var enumSb = new StringBuilder(81_920);
-		enumSb.AppendLine("namespace TablerIconGenerator;");
-		enumSb.AppendLine("public enum TablerIcon");
-		enumSb.AppendLine("{");
-
-		var extensionsSb = new StringBuilder(81_920 * 6);
-		extensionsSb.AppendLine("namespace TablerIconGenerator;");
-		extensionsSb.AppendLine("public static class TablerIconExtensions");
-		extensionsSb.AppendLine("{");
-
-		var extensionsMethodSb = new StringBuilder(10_000);
-		extensionsMethodSb.AppendLine("public static Microsoft.AspNetCore.Components.RenderFragment ToRenderFragment(this TablerIconGenerator.TablerIcon icon) => icon switch");
-		extensionsMethodSb.AppendLine("{");
-		
-		foreach (var assemblyName in iconAssrmblyNames)
-		{
-			var iconFormattedName = GetIconFilteredName(assemblyName);
-
-			if (string.IsNullOrEmpty(iconFormattedName))
-				continue;
-
-			enumSb.Append(iconFormattedName);
-			enumSb.Append(",\n");
-
-			extensionsSb.AppendLine(CreateRenderFragment(assembly, assemblyName, iconFormattedName));
-			extensionsMethodSb.AppendLine($"TablerIconGenerator.TablerIcon.{iconFormattedName} => {iconFormattedName},");
-		}
-
-		enumSb.Remove(enumSb.Length - 2, 2);
-		enumSb.AppendLine("}");
-
-		var enumSource = enumSb.ToString();
-		context.AddSource("TablerIcon.g.cs", SourceText.From(enumSource, Encoding.UTF8));
-
-		extensionsMethodSb.AppendLine("};");
-		extensionsSb.Append(extensionsMethodSb);
-
-		extensionsSb.AppendLine("}");
-		var extensionsSource = extensionsSb.ToString();
-
-		context.AddSource("TablerIconExtensions.g.cs", SourceText.From(extensionsSource, Encoding.UTF8));
-	}
 
 	private static string GetIconFilteredName(string iconName)
 	{
@@ -129,7 +138,7 @@ public class IconGenerator : ISourceGenerator
 		var svg = new XmlDocument();
 		svg.Load(stream);
 
-		var content = svg.DocumentElement.InnerXml.Replace("\r\n", "").Replace("\n", "").Replace("\r", "").Replace("\"", "\"\"");
+		var content = svg.DocumentElement.InnerXml;
 		var viewBox = svg.DocumentElement.GetAttribute("viewBox");
 		var strokeWidth = svg.DocumentElement.GetAttribute("stroke-width");
 		var strokeLineCap = svg.DocumentElement.GetAttribute("stroke-linecap");
@@ -142,6 +151,7 @@ public class IconGenerator : ISourceGenerator
 					       
 					       builder.OpenElement(seq++, "svg");
 					       
+					       builder.AddAttribute(seq++, "style", "width: 1em;");
 					       builder.AddAttribute(seq++, "focusable", false);
 					       builder.AddAttribute(seq++, "viewBox", "{{viewBox}}");
 					       builder.AddAttribute(seq++, "aria-hidden", true);
