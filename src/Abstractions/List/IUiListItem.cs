828baa715ae4cf635fda85c708e8467e7fcf206c
Ivan Dmitriev <42055372+IvanDmitriev1@users.noreply.github.com>
2023-10-08 10:38:18 +0600

    Added UiListItemBase

--- src/Abstractions/List/IUiListItem.cs
+++ src/Abstractions/List/IUiListItem.cs
@@ -4,8 +4,7 @@ public interface IUiListItem<T> : IDisposable
     where T : notnull
 {
     T Value { get; set; }
-    RenderFragment ChildContent { get; set; }
-
+    
     void SetSelected();
     void SetUnselected();
 }
---
+++ src/BaseComponents/UiListItemBase.cs
@@ -0,0 +1,50 @@
+﻿using UiKit.Abstractions.List;
+
+namespace UiKit.BaseComponents;
+
+public abstract class UiListItemBase<T> : UiKitRenderComponentBase, IUiListItem<T>
+    where T : notnull
+{
+    [Parameter, EditorRequired]
+    public T Value { get; set; } = default!;
+
+    [CascadingParameter]
+    protected IUiList<T>? List { get; set; }
+
+    protected bool Selected
+    {
+        get => _selected;
+        private set
+        {
+            if (_selected == value)
+                return;
+            
+            _selected = value;
+            AllowRender();
+        }
+    }
+
+    private bool _selected;
+    
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
+
+        List?.UnRegisterListItem(this);
+    }
+    
+    protected override void OnFirstRender()
+    {
+        List?.RegisterListItem(this);
+    }
+    
+    public void SetSelected()
+    {
+        Selected = true;
+    }
+
+    public void SetUnselected()
+    {
+        Selected = false;
+    }
+}
