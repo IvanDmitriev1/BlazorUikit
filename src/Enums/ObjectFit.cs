68156aa2c3dead47990d38b2fca4381475b63063
Ivan Dmitriev <42055372+IvanDmitriev1@users.noreply.github.com>
2023-10-08 16:49:25 +0600

    Added ObjectFit

---
+++ src/Enums/ObjectFit.cs
@@ -0,0 +1,23 @@
+﻿namespace UiKit.Enums;
+
+public enum ObjectFit
+{
+    Contain,
+    Cover,
+    Fill,
+    None,
+    ScaleDown
+}
+
+public static class ObjectFitExtensions
+{
+    public static string ToTailwindCss(this ObjectFit objectFit) => objectFit switch
+    {
+        ObjectFit.Contain => "object-contain",
+        ObjectFit.Cover =>"object-cover",
+        ObjectFit.Fill => "object-fill",
+        ObjectFit.None => "object-none",
+        ObjectFit.ScaleDown => "object-scale-down",
+        _ => throw new ArgumentOutOfRangeException(nameof(objectFit), objectFit, null)
+    };
+}
