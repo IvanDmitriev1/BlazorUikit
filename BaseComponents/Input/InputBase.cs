b845478e7a258666eb1af21998c03eadcb2a461d
Ivan Dmitriev <42055372+IvanDmitriev1@users.noreply.github.com>
2023-11-27 22:55:53 +0600

    Added UserAttributes to the InputBase

--- BaseComponents/Input/InputBase.cs
+++ BaseComponents/Input/InputBase.cs
@@ -2,6 +2,13 @@
 
 public abstract class InputBase<T> : ValidationComponentBase<T>
 {
+    /// <summary>
+    /// UserAttributes carries all attributes you add to the component that don't match any of its parameters.
+    /// They will be splatted onto the underlying HTML tag.
+    /// </summary>
+    [Parameter(CaptureUnmatchedValues = true)]
+    public Dictionary<string, object>? UserAttributes { get; set; }
+
     [Parameter]
     public bool Disabled { get; set; }
 
