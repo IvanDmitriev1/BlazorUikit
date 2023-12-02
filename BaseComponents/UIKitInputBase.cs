d951bf87bb3fc1b1e710a46642f2c5895124eda7
Ivan Dmitriev <42055372+IvanDmitriev1@users.noreply.github.com>
2023-12-02 19:39:01 +0600

    Implemented TextField

---
+++ BaseComponents/UIKitInputBase.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace BlazorUiKit.BaseComponents;
+
+public abstract class UIKitInputBase<T> : InputBase<T>
+{
+	[Parameter]
+	public string Class { get; set; } = string.Empty;
+
+	[Parameter]
+	public bool Disabled { get; set; }
+
+	[Parameter]
+	public bool ReadOnly { get; set; }
+
+	[Parameter]
+	public string? Label { get; set; }
+
+	[Parameter]
+	public string? Placeholder { get; set; }
+
+	private bool _render;
+	private bool _firstRendered;
+
+	protected override bool ShouldRender()
+	{
+		if (!_firstRendered)
+			return true;
+
+		if (!_render)
+			return false;
+
+		_render = false;
+		return true;
+	}
+
+	protected override void OnAfterRender(bool firstRender)
+	{
+		if (firstRender)
+			_firstRendered = true;
+
+		base.OnAfterRender(firstRender);
+	}
+
+	protected void AllowRender()
+	{
+		_render = true;
+	}
+
+	protected void StateHasChangedWithRendering()
+	{
+		if (_render)
+			return;
+
+		AllowRender();
+		StateHasChanged();
+	}
+}
